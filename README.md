81b8d00bc4a96099309fd8d9b944cbe1b254bc69
Sakhmina <asadullaevasakhmina@gmail.com>
2022-03-30 00:05:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# netology_javacore_network_hosts
+Working with hosts file, client-server application with 3-5 steps 
+## Задача "Клиент-сервер с рюшечками"
+Код по [ссылке](https://github.com/A-Sakhmina/netology_javacore_network_hosts.git)
+### Задание 
+В серверной и клиентской части сделайте последовательность вызовов в рамках одной сессии таким образом, 
+чтобы получился некий **диалог между клиентом и сервером**, содержащий от 3 до 5 (или больше) шагов. 
+
+Например,
+* при подключении сервер спрашивает: `Write your name`. Клиент читает это сообщение и отправляет ему имя.
+* Сервер читает имя, запоминает его, и задает следующий вопрос: `Are you child? (yes/no)`. Клиент отвечает `yes` или `no`.
+* На что сервер, если увидел `yes`, отвечает `Welcome to the kids area, %username%! Let's play!`, 
+* а если `no`, то `Welcome to the adult zone, %username%! Have a good rest, or a good working day!` 
+(где %username% - имя клиента из предыдущего шага)
---
+++ src/main/java/Client.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        //localhost
+        String host = "netology.homework";
+        int port = 8080;
+
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             Scanner scanner = new Scanner(System.in)) {
+            //считываем сообщение с сервера
+            System.out.println(in.readLine());
+            //ввод ответного сообщения с консоли
+            String name = scanner.nextLine();
+            //отправка ответного сообщения, введенного в консоли, на сервер
+            out.println(name);
+
+            System.out.println(in.readLine());
+            String isChild = scanner.nextLine();
+            out.println(isChild);
+            System.out.println(in.readLine());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
